f5a8c2938b94acc2b1d3556d82d3b730d44cf2e9
Igor Meshalkin <770190@bk.ru>
2022-02-21 09:57:19 +1000

    Implemented JdbcMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,37 +1,70 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.Util;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
 
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcMealRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew()) {
+            jdbcTemplate.update("INSERT INTO meals (user_id, dateTime, description, calories) VALUES (?, ?, ?, ?)"
+                    , userId, meal.getDateTime(), meal.getDescription(), meal.getCalories());
+        } else {
+            jdbcTemplate.update("UPDATE meals SET datetime = ?, description = ?, calories= ? WHERE id = ?;"
+                    , meal.getDateTime(), meal.getDescription(), meal.getCalories(), meal.getId());
+            return meal;
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id = ?", id) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE id = ?", ROW_MAPPER, id).stream()
+                .findAny().orElse(null);
+//        List<Meal> meals = jdbcTemplate.query("SELECT * FROM meals WHERE (id = ?, user_id = ?)", ROW_MAPPER, id, userId);
+//        return DataAccessUtils.singleResult(meals);
     }
+
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ?", ROW_MAPPER, userId).stream()
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ?", ROW_MAPPER, userId).stream()
+                .filter(meal -> Util.isBetweenHalfOpen(meal.getDateTime(), startDateTime, endDateTime))
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed()).collect(Collectors.toList());
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,15 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, dateTime, description, calories)
+VALUES (100000, '2021-12-20 20:20:22', 'Обед', 600),
+       (100001, '2021-12-20 20:45:22', 'Завтрак', 680),
+       (100002, '2021-12-20 20:13:22', 'Ужин', 640),
+       (100000, '2021-12-22 20:20:22', 'Обед', 600),
+       (100001, '2021-12-21 20:45:22', 'Завтрак', 680),
+       (100002, '2021-12-23 20:13:22', 'Ужин', 640),
+       (100000, '2021-12-21 20:20:22', 'Обед', 600),
+       (100001, '2021-12-23 20:45:22', 'Завтрак', 680),
+       (100002, '2021-12-26 20:13:22', 'Ужин', 640);
+
